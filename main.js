c2529f8732be923fb971046428b73ae94156ac0d
sivanildooliveira <sivanildoseguro@outlook.com.br>
2023-05-29 22:26:35 -0300

    import do celular

--- main.js
+++ main.js
@@ -10,3 +10,47 @@ header.querySelectorAll('a').forEach((lk)=>{
         header.classList.toggle('active')
     })
 })
+
+
+
+/*    document.addEventListener('DOMContentLoaded', function() {
+        var links = document.getElementsByClassName('link-secao');
+        var headerHeight = 80; // Altura do cabeçalho em pixels
+
+        for (var i = 0; i < links.length; i++) {
+            links[i].addEventListener('click', function(event) {
+                event.preventDefault();
+
+                var href = this.getAttribute('href');
+                var element = document.querySelector(href);
+                var offsetTop = element.offsetTop;
+                var adjustedOffset = offsetTop - headerHeight;
+
+                window.scrollTo({
+                    top: adjustedOffset,
+                    behavior: 'smooth'
+                });
+            });
+        }
+    });
+
+
+  */
+  
+document.addEventListener('DOMContentLoaded', function() {
+  var links = document.querySelectorAll('header #menuPri a');
+
+  links.forEach((link)=>{
+    link.addEventListener('click', function(event) {
+      event.preventDefault();
+
+      var href = this.getAttribute('href');
+      var offsetTop = document.querySelector(href).offsetTop;
+
+      window.scrollTo({
+        top: offsetTop,
+        behavior: 'smooth'
+      })
+    })
+  })
+});
