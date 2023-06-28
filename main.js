d8f0618bcac3979fec6fde51f7d3aa205b510628
sivanildooliveira <sivanildoseguro@outlook.com.br>
2023-06-28 17:17:17 -0300

    att

--- main.js
+++ main.js
@@ -8,23 +8,3 @@ header.querySelectorAll('a').forEach((lk)=>{
         header.classList.toggle('active')
     })
 })
-
-
-var verMais = (id)=>{
-  document.getElementById(id).showModal()
-}
-
-/*document.addEventListener('DOMContentLoaded', function() {
-  var links = document.querySelectorAll('header #menuPri a');
-
-  links.forEach((link)=>{
-    link.addEventListener('click', function(event) {
-      event.preventDefault()
-
-      var href = this.getAttribute('href');
-      var offsetTop = document.querySelector(href).offsetTop;
-    
-      window.scrollTo({ top: offsetTop, behavior: 'smooth'})
-    })
-  })
-});*/
