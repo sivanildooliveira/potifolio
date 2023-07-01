4a0309175403f64b81813768fe116612d4fe0ee7
sivanildooliveira <sivanildoseguro@outlook.com.br>
2023-07-01 09:34:23 -0300

    attr

--- main.js
+++ main.js
@@ -8,3 +8,24 @@ header.querySelectorAll('a').forEach((lk)=>{
         header.classList.toggle('active')
     })
 })
+
+document.addEventListener('DOMContentLoaded', function() {
+    const smoothScrollLinks = document.querySelectorAll('.smooth-scroll');
+  
+    for (let link of smoothScrollLinks) {
+      link.addEventListener('click', function(e) {
+        e.preventDefault();
+  
+        const targetId = this.getAttribute('href');
+        const targetElement = document.querySelector(targetId);
+  
+        if (targetElement) {
+          window.scrollTo({
+            top: targetElement.offsetTop,
+            behavior: 'smooth'
+          });
+        }
+      });
+    }
+  });
+  
