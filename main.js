8d311a83cd6694e078f0a7196d86f97b6a44393e
sivanildooliveira <sivanildoseguro@outlook.com.br>
2023-07-02 10:59:34 -0300

    footer

--- main.js
+++ main.js
@@ -9,23 +9,12 @@ header.querySelectorAll('a').forEach((lk)=>{
     })
 })
 
-document.addEventListener('DOMContentLoaded', function() {
-    const smoothScrollLinks = document.querySelectorAll('.smooth-scroll');
-  
-    for (let link of smoothScrollLinks) {
-      link.addEventListener('click', function(e) {
-        e.preventDefault();
-  
-        const targetId = this.getAttribute('href');
-        const targetElement = document.querySelector(targetId);
-  
-        if (targetElement) {
-          window.scrollTo({
-            top: targetElement.offsetTop,
-            behavior: 'smooth'
-          });
-        }
-      });
-    }
-  });
-  
+
+var potfolios = document.querySelectorAll('#projetos ul li')
+
+potfolios.forEach((el)=>{
+  el.addEventListener('click', ()=>{
+    potfolios.forEach((rem)=>{rem.classList.remove('active')})
+    el.classList.add('active')
+  })
+})
