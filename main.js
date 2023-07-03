a0495bdcc5dca1017f50fedb068db0c77e8ab9fd
sivanildooliveira <sivanildoseguro@outlook.com.br>
2023-07-02 21:21:49 -0300

    attrs

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
 var btnmenu = document.getElementById('btnMenu');
 var header = document.querySelector('header');
-btnmenu.addEventListener('click', ()=>{
+const menu = document.getElementById('menuPri');
+
+btnmenu.addEventListener('click', (jh)=>{
     header.classList.toggle('active')
 })
-header.querySelectorAll('a').forEach((lk)=>{
+
+menu.querySelectorAll('a').forEach((lk)=>{
     lk.addEventListener('click', ()=>{
         header.classList.toggle('active')
     })
@@ -14,7 +17,47 @@ var potfolios = document.querySelectorAll('#projetos ul li')
 
 potfolios.forEach((el)=>{
   el.addEventListener('click', ()=>{
-    potfolios.forEach((rem)=>{rem.classList.remove('active')})
+    potfolios.forEach((info)=>{info.classList.remove('active')})
     el.classList.add('active')
   })
 })
+
+
+const salvarMensagem = ()=>{
+  const form = document.getElementById('formMensage');
+
+  let nome = form.querySelector('#nome')
+  let email = form.querySelector('#email')
+  let telefone = form.querySelector('#telefone')
+  let titulo = form.querySelector('#titulo')
+  let mensagem = form.querySelector('#mensagem')
+  const id = new Date()
+  const data = {
+    'nome': nome,
+    'email': email,
+    'telefone': telefone,
+    'titulo': titulo,
+    'mensagem': mensagem
+  };
+
+  alert('a mensagem não foi enviada')
+
+  /*fetch('https://mybase-c9c2d-default-rtdb.asia-southeast1.firebasedatabase.app/mensagens/.json', {
+    method: 'POST',
+    body: JSON.stringify(data)
+  })
+    .then(response => {
+      console.log(response)
+      if (!response.ok) {
+        throw new Error('Erro na requisição POST');
+      }
+      return response.json();
+    })
+    .then(data => {
+      console.log(data);
+    })
+    .catch(error => {
+      console.error(error);
+    });*/
+  
+}
