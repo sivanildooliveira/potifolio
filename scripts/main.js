83801cfe7aa810dd1f1ab08681b10abd82763ef3
sivanildooliveira <110950356+sivanildooliveira@users.noreply.github.com>
2023-07-03 19:11:47 -0300

    Add files via upload

--- scripts/main.js
+++ scripts/main.js
@@ -23,41 +23,16 @@ potfolios.forEach((el)=>{
 })
 
 
-const salvarMensage = ()=>{
-  const form = document.getElementById('formMensage');
-
-  let nome = form.querySelector('#nome')
-  let email = form.querySelector('#email')
-  let telefone = form.querySelector('#telefone')
-  let titulo = form.querySelector('#titulo')
-  let mensagem = form.querySelector('#mensagem')
-  const id = new Date()
-  const data = {
-    'nome': nome,
-    'email': email,
-    'telefone': telefone,
-    'titulo': titulo,
-    'mensagem': mensagem
-  };
-
-  alert('a mensagem não foi enviada')
-
-  /*fetch('https://mybase-c9c2d-default-rtdb.asia-southeast1.firebasedatabase.app/mensagens/.json', {
-    method: 'POST',
-    body: JSON.stringify(data)
-  })
-    .then(response => {
-      console.log(response)
-      if (!response.ok) {
-        throw new Error('Erro na requisição POST');
-      }
-      return response.json();
-    })
-    .then(data => {
-      console.log(data);
-    })
-    .catch(error => {
-      console.error(error);
-    });*/
+var openVerMaisSobre = (btn)=>{
+  const texto = document.getElementById('maisSobre');
   
+  
+  texto.classList.toggle('active')
+  btn.classList.toggle('active')
+  
+  if(btn.classList.contains('active')){
+    btn.innerHTML = '<i class="bi bi-arrow-bar-up"></i><br>ver menos'
+  }else{
+    btn.innerHTML = 'ver mais<br><i class="bi bi-arrow-bar-down"></i>'
+  }
 }
